e2c4f441b72dff02e9993250b22e66e782d9bb09
Ada Richmond <devenger@gmail.com>
2024-06-16 14:28:39 +0100

    fix health notches scaling when health lost

--- scripts/HealthBar.gd
+++ scripts/HealthBar.gd
@@ -1,15 +1,16 @@
 extends Node2D
 
 @onready var health_remaining_rect: ColorRect = $HealthRemainingRect
+@onready var health_back_rect: ColorRect = $HealthBackRect
 var player: Player
 
 func _draw():
 	if not player:
 		return
 	for notch in range(1, player.max_health):
-		var x_pos: int = health_remaining_rect.position.x + lerp(0, int(health_remaining_rect.size.x), float(notch) / player.max_health)
-		var from := Vector2(x_pos, health_remaining_rect.position.y)
-		var to := Vector2(x_pos, health_remaining_rect.position.y + health_remaining_rect.size.y / 2)
+		var x_pos: int = health_back_rect.position.x + lerp(0, int(health_back_rect.size.x), float(notch) / player.max_health)
+		var from := Vector2(x_pos, health_back_rect.position.y)
+		var to := Vector2(x_pos, health_back_rect.position.y + health_back_rect.size.y / 2)
 		draw_line(from, to, Color.BLACK, 2, false)
 
 
